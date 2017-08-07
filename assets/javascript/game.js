b0844b4d1cd3e2f2e8ddc94993f714bb11e96d49
mark7389 <mark.soliman73@gmail.com>
2017-08-06 20:32:06 -0400

    phase 2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,47 @@
+
+var character = {
+
+	name:"",
+	hp:150,
+	attack:"",
+	counterattack:"",
+	frame:"",
+
+	chooseHero: function(){
+
+		this.frame.on("click", function(){
+			
+			$(this).appendTo("#hero");
+			$("<div>").text("You picked Aladdin !").appendTo("#hero");
+			$("<button>").addClass("btn btn-default primary attack").text("Strike").appendTo("#hero");		
+		});
+		
+	},
+
+	chooseOpponent: function(){
+
+		this.frame.on("click", function(){
+			
+			$(this).appendTo("#hero");
+			$("<div>").text("You picked Jafar !").appendTo("#opponent");
+					
+		});
+
+	},
+
+}
+
 $(document).ready(function(){
 
-$(".characterframe").on("click", function(){
+var aladdin = Object.create(character);
+var gazeem = Object.create(character);
+var jafar = Object.create(character);
+var genie = Object.create(character);
+aladdin.frame = $("<div>").addClass("characterframe circle aladdin").text(aladdin.hp).appendTo($("#ether"));
+gazeem.frame = $("<div>").addClass("characterframe circle gazeem").text(gazeem.hp).appendTo($("#ether"));
+jafar.frame = $("<div>").addClass("characterframe circle jafar").text(jafar.hp).appendTo($("#ether"));
+genie.frame = $("<div>").addClass("characterframe circle genie").text(genie.hp).appendTo($("#ether"));			
 
-	$("#hero").html($(this));
-	$("#opponent").html($(this));
-});
-
+aladdin.chooseHero();
+jafar.chooseOpponent();
 });
